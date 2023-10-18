b702c1867e8297033cdca3158f2716cd0c253142
Eryani <eryani@Eryanis-MacBook-Pro.local>
2023-10-18 17:39:21 +0200

    added search functionality

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,8 +2,7 @@
     @include('_posts-header')
 
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-        @if(count($posts))
-
+        @if($posts->count()>0)
             <x-posts-grid :posts=" $posts"/>
         @else
             <h3 class="text-center">There is no blog to show</h3>
