1c103b58d54ecc9bcde8dba2d98e335b7d477d36
Eryani <eryani@Eryanis-MacBook-Pro.local>
2023-10-23 15:13:55 +0200

    fixed the error in build http query

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,7 +2,6 @@
 
     namespace App\Http\Controllers;
 
-    use App\Models\Category;
     use App\Models\Post;
 
     class PostController extends Controller
@@ -11,8 +10,7 @@
         function index()
         {
             return view('posts.index', [
-                'posts'      => Post::latest()->filter(request(['search', 'category', 'author']))->get(),
-                'categories' => Category::all()
+                'posts' => Post::latest()->filter(request(['search', 'category', 'author']))->get(),
             ]);
         }
 
