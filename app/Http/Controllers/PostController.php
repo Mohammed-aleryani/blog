011703e5ea1305a8414be7284286cd598ca6484e
Eryani <eryani@Eryanis-MacBook-Pro.local>
2023-10-23 15:55:30 +0200

    added pagination functionality

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,7 +10,7 @@
         function index()
         {
             return view('posts.index', [
-                'posts' => Post::latest()->filter(request(['search', 'category', 'author']))->get(),
+                'posts' => Post::latest()->filter(request(['search', 'category', 'author']))->paginate(6),
             ]);
         }
 
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,6 +4,8 @@
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
         @if($posts->count()>0)
             <x-posts-grid :posts=" $posts"/>
+            {{$posts->links()}}
+
         @else
             <h3 class="text-center">There is no blog to show</h3>
         @endif
