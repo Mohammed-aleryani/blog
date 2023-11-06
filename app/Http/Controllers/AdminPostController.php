646a777137bfabd46a30bbdf2116f95275c609f5
Eryani <mym.aleryani@gmail.com>
2023-11-06 14:14:22 +0100

    use schedule and model prun to delete draft

--- app/Http/Controllers/AdminPostController.php
+++ app/Http/Controllers/AdminPostController.php
@@ -1,98 +1,93 @@
 <?php
 
-    namespace App\Http\Controllers;
+namespace App\Http\Controllers;
 
-    use App\Models\Category;
-    use App\Models\Post;
-    use Illuminate\Support\Carbon;
-    use Illuminate\Validation\Rule;
+use App\Models\Category;
+use App\Models\Post;
+use Illuminate\Validation\Rule;
 
-    class AdminPostController extends Controller
+class AdminPostController extends Controller
+{
+
+    public
+    function index()
     {
-        public
-        function index()
-        {
-            $stale_posts = Post::where('status', 'draft')->where('updated_at', '<', Carbon::now()->subWeek(4))->get();
 
-            foreach ($stale_posts as $post) {
-                $post->delete();
-            }
-            return view('admin.posts.index', [
-                'posts' => Post::orderBy('id', 'desc')->paginate(50)
-            ]);
-        }
-
-        public
-        function create()
-        {
-            return view('admin.posts.create', ['categories' => Category::all()]);
-        }
-
-        public
-        function store()
-        {
-            $attributes = $this->validatePost();
-
-            if (isset(request()->thumbnail)) {
-                $attributes[ 'thumbnail' ] = request()->file('thumbnail')->store('thumbnails');
-            }
-            $attributes[ 'user_id' ] = auth()->id();
-            Post::create($attributes);
-            return redirect('/')->with('success', 'Your post has been added!');
-        }
-
-        public
-        function edit(
-            Post $post
-        ){
-            return view('admin.posts.edit', [
-                'post'       => $post,
-                'categories' => Category::all()
-            ]);
-        }
-
-        public
-        function update(
-            Post $post
-        ){
-            $attributes = $this->validatePost($post);
-
-
-            if (($attributes[ 'thumbnail' ] ?? false)) {
-                $attributes[ 'thumbnail' ] = request()->file('thumbnail')->store('thumbnails');
-            }
-
-            $post->update($attributes);
-            return back()->with('success', 'Post Updated!');
-        }
-
-        public
-        function delete(
-            Post $post
-        ){
-            $post->delete();
-            return back()->with('success', 'Post deleted!');
-        }
-
-        /**
-         * @param  Post  $post
-         * @return array
-         */
-        protected
-        function validatePost(
-            ?Post $post = null
-        )
-        : array{
-            $post ??= new Post();
-
-            return request()->validate([
-                'title'       => 'required',
-                'thumbnail'   => ['image'],
-                'slug'        => ['required', Rule::unique('posts', 'slug')->ignore($post)],
-                'excerpt'     => 'required',
-                'body'        => 'required',
-                'status'      => 'required',
-                'category_id' => ['required', Rule::exists('categories', 'id')]
-            ]);
-        }
+        return view('admin.posts.index', [
+            'posts' => Post::orderBy('id', 'desc')->paginate(50)
+        ]);
     }
+
+    public
+    function create()
+    {
+        return view('admin.posts.create', ['categories' => Category::all()]);
+    }
+
+    public
+    function store()
+    {
+        $attributes = $this->validatePost();
+
+        if (isset(request()->thumbnail)) {
+            $attributes['thumbnail'] = request()->file('thumbnail')->store('thumbnails');
+        }
+        $attributes['user_id'] = auth()->id();
+        Post::create($attributes);
+        return redirect('admin/posts')->with('success', 'Your post has been added!');
+    }
+
+    public
+    function edit(
+        Post $post
+    ) {
+        return view('admin.posts.edit', [
+            'post' => $post,
+            'categories' => Category::all()
+        ]);
+    }
+
+    public
+    function update(
+        Post $post
+    ) {
+        $attributes = $this->validatePost($post);
+
+
+        if (($attributes['thumbnail'] ?? false)) {
+            $attributes['thumbnail'] = request()->file('thumbnail')->store('thumbnails');
+        }
+
+        $post->update($attributes);
+        return redirect('admin/posts')->with('success', 'Post Updated!');
+    }
+
+    public
+    function delete(
+        Post $post
+    ) {
+        $post->delete();
+        return back()->with('success', 'Post deleted!');
+    }
+
+    /**
+     * @param  Post  $post
+     * @return array
+     */
+    protected
+    function validatePost(
+        ?Post $post = null
+    ): array {
+        $post ??= new Post();
+
+        return request()->validate([
+            'title' => 'required',
+            'thumbnail' => ['image'],
+            'slug' => ['required', Rule::unique('posts', 'slug')->ignore($post)],
+            'excerpt' => 'required',
+            'body' => 'required',
+            'status' => 'required',
+            'category_id' => ['required', Rule::exists('categories', 'id')]
+        ]);
+    }
+}
--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -6,37 +6,37 @@
                     <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
                         <table class="min-w-full divide-y divide-gray-200">
                             <tbody class="bg-white divide-y divide-gray-200">
-                                @foreach ($posts as $post)
-                                    <tr>
-                                        <td class="px-6 py-4 whitespace-nowrap">
-                                            <div class="flex items-center">
-                                                <div class="text-sm font-medium text-gray-900">
-                                                    <a href="/posts/{{ $post->slug }}">
-                                                        {{ $post->title }}
-                                                    </a>
-                                                </div>
+                            @foreach ($posts as $post)
+                                <tr>
+                                    <td class="px-6 py-4 whitespace-nowrap">
+                                        <div class="flex items-center">
+                                            <div class="text-sm font-medium text-gray-900">
+                                                <a href="/posts/{{ $post->slug }}">
+                                                    {{ $post->title }}
+                                                </a>
                                             </div>
-                                        </td>
+                                        </div>
+                                    </td>
 
-                                        <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                                            <p
-                                                    class="text-blue-500 hover:text-blue-600">{{$post->save_as}}</p>
-                                        </td>
-                                        <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                                            <a href="/admin/posts/{{ $post->id }}/edit"
-                                               class="text-blue-500 hover:text-blue-600">Edit</a>
-                                        </td>
+                                    <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                                        <p
+                                            class="text-blue-500 hover:text-blue-600 uppercase">{{$post->status}}</p>
+                                    </td>
+                                    <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                                        <a href="/admin/posts/{{ $post->id }}/edit"
+                                           class="text-blue-500 hover:text-blue-600">Edit</a>
+                                    </td>
 
-                                        <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                                            <form method="POST" action="/admin/posts/{{ $post->id }}">
-                                                @csrf
-                                                @method('DELETE')
+                                    <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                                        <form method="POST" action="/admin/posts/{{ $post->id }}">
+                                            @csrf
+                                            @method('DELETE')
 
-                                                <button class="text-xs text-gray-400">Delete</button>
-                                            </form>
-                                        </td>
-                                    </tr>
-                                @endforeach
+                                            <button class="text-xs text-gray-400">Delete</button>
+                                        </form>
+                                    </td>
+                                </tr>
+                            @endforeach
                             </tbody>
                         </table>
                     </div>
